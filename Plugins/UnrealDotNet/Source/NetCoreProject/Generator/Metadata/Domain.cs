a19d3c92762e28b307ab5dd975479091c6194ec2
mrkriv <virus_450@mail.ru>
2017-08-23 00:20:37 +0300

    Цветной вывод метаданных в консольку

--- Plugins/UnrealDotNet/Source/NetCoreProject/Generator/Metadata/Domain.cs
+++ Plugins/UnrealDotNet/Source/NetCoreProject/Generator/Metadata/Domain.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Generator.Metadata
 {
@@ -23,7 +24,7 @@ namespace Generator.Metadata
                 {
                     Console.ForegroundColor = ConsoleColor.DarkGreen;
                     Console.Write(" base of ");
-                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = cl.BaseClass.IsImplemented ? ConsoleColor.Cyan : ConsoleColor.DarkRed;
                     Console.Write(cl.BaseClass.Name);
                 }
 
@@ -35,16 +36,79 @@ namespace Generator.Metadata
 
                 Console.WriteLine();
 
-                Console.ForegroundColor = ConsoleColor.DarkGray;
                 foreach (var method in cl.Methods)
                 {
-                    Console.WriteLine("\t" + method);
+                    PrintMehod(method);
                 }
 
                 Console.WriteLine();
             }
 
             Console.ResetColor();
+        }
+
+        private static void PrintMehod(Method method)
+        {
+            var ext = new List<string>();
+
+            if (method.IsConst) ext.Add("const");
+            if (method.IsVirtual) ext.Add("virtual");
+
+            Console.Write("\t");
+            PrintVariable(method.ReturnType);
+
+            Console.ResetColor();
+            Console.Write($" {method.Name} (");
+
+            for (var i = 0; i < method.InputTypes.Count; i++)
+            {
+                if (i != 0)
+                    Console.Write(", ");
+
+                PrintVariable(method.InputTypes[i]);
+            }
+
+            Console.Write(")");
+
+            if (ext.Any())
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.Write(" " + string.Join(", ", ext));
+            }
+
+            Console.WriteLine();
+        }
+
+        private static void PrintVariable(Variable variable)
+        {
+            var ext = new List<string>();
+
+            if (variable.IsConst) ext.Add("const");
+            if (variable.IsReference) ext.Add("ref");
+            if (variable.IsPointer) ext.Add("ptr");
+
+            if (ext.Any())
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.Write(string.Join(" ", ext) + " ");
+            }
+
+            var clVar = variable as ClassVariable;
+            Console.ForegroundColor = clVar?.ClassType.IsImplemented == false ? ConsoleColor.DarkRed : ConsoleColor.Cyan;
+
+            Console.Write(variable.Type);
+
+            if (!string.IsNullOrEmpty(variable.Name))
+            {
+                Console.ResetColor();
+                Console.Write(" " + variable.Name);
+            }
+
+            if (!string.IsNullOrEmpty(variable.Default))
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.Write(" = " + variable.Default);
+            }
         }
     }
 }
